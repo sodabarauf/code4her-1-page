dc400fe6b7b980d27726d278c41f1ee99993c217
sodaba rauf <sodabarauf4@gmail.com>
2025-04-08 13:52:39 +0430

    added an array of objects

---
+++ src/data/jobs.js
@@ -0,0 +1,59 @@
+const jobs = [
+  {
+    id: 1,
+    company: "Netflix",
+    logo: "/logos/netflix.png",
+    title: "Backend .Net Developer",
+    tags: ["Fullstack", "Angular", "Azure", "C#"],
+    salary: "$13k/month",
+    time: "6 mins ago"
+  },
+  {
+    id: 2,
+    company: "Google",
+    logo: "/logos/google.png",
+    title: "Mid Fullstack Developer",
+    tags: ["Fullstack", "Angular", "Azure", "C#"],
+    salary: "$15k/month",
+    time: "18 mins ago"
+  },
+  {
+    id: 3,
+    company: "Dropbox",
+    logo: "/logos/dropbox.png",
+    title: "Full Stack Developer",
+    tags: ["Fullstack", "React", "Node.js", "JavaScript"],
+    salary: "$9k/month",
+    time: "24 mins ago"
+  },
+  {
+    id: 4,
+    company: "Apple",
+    logo: "/logos/apple.png",
+    title: "IT Software Consultant",
+    tags: ["C#", "SQL", "Azure"],
+    salary: "$10k/month",
+    time: "1 hour ago"
+  },
+  {
+    id: 5,
+    company: "IBM",
+    logo: "/logos/ibm.png",
+    title: ".NET Developer",
+    tags: ["Backend", ".NET", ".NET CORE"],
+    salary: "$15k/month",
+    time: "2 hours ago"
+  },
+  {
+    id: 6,
+    company: "Facebook",
+    logo: "/logos/facebook.png",
+    title: "Platforms Engineer",
+    tags: ["DevOps", "Ansible", "Linux", "Networks"],
+    salary: "$7k/month",
+    time: "1 day ago"
+  }
+];
+
+export default jobs;
+  
